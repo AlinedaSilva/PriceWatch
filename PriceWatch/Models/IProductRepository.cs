7e38d442adabaf90fbe315c2b44412550c6aaa0e
AlinedaSilva <35935280+AlinedaSilva@users.noreply.github.com>
2018-12-04 10:43:44 +0000

    Adding IProductRepository

---
+++ PriceWatch/Models/IProductRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PriceWatch.Models
+{
+    interface IProductRepository
+    {
+        Task<IEnumerable<ProductViewModel>> GetProductsAsync(string query, int offset, int limit = 10);
+    }
+}
+
+
