241bcda33307a50cda5369116a0c146ca2c4d2c1
AlinedaSilva <35935280+AlinedaSilva@users.noreply.github.com>
2018-12-04 11:24:57 +0000

    adding PriceWatchContext and DAL folder

---
+++ PriceWatch/DAL/PriceWatchContext.cs
@@ -0,0 +1,26 @@
+﻿using PriceWatch.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+using System.Data.Entity.Infrastructure;
+namespace PriceWatch.DAL
+{
+    public class PriceWatchContext : DbContext
+    {
+        public PriceWatchContext() : base("PriceWatchContext")
+        {
+
+        }
+       
+        public DbSet<PriceWatchViewModel> PriceWatches {get;set;}
+
+        public DbSet<PriceWatchEntry> PriceWatchEntries { get; set; }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
