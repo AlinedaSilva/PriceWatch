482e8d45f603d918572f95f70c71f4973c5719cc
AlinedaSilva <35935280+AlinedaSilva@users.noreply.github.com>
2018-12-04 11:00:08 +0000

    Adding details for PriceWatchEntry

--- PriceWatch/Models/PriceWatchEntry.cs
+++ PriceWatch/Models/PriceWatchEntry.cs
@@ -1,6 +1,19 @@
-﻿namespace PriceWatch.Models
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PriceWatch.Models
 {
+    public enum PriceIndicator
+    {
+        Up, Down, Same
+    }
     public class PriceWatchEntry
     {
+        public long Id { get; set; } // id for PriceWatchEntry
+        public DateTime Date { get; set; }
+        public decimal Price { get; set; }
+        public PriceIndicator PriceIndicator { get; set; }
     }
 }
